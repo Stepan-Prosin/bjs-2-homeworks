67ab800b8c2d644fb15dca5a238f9e214dbe1b67
stepan TeSt <stepan.prosin@mail.ru>
2024-05-17 16:02:17 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age);
+	if (result.length === 0) {
+		return 0;
+	}
+	return result.reduce((acc, item) => acc + item, 0) / result.length;
 }
